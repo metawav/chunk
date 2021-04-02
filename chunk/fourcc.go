71fdf908b4932e8049cc61fa795d075b870a1adc
Patrick Moule <dev@patrickmoule.net>
2021-04-02 15:19:19 +0200

    docs: docs added

--- chunk/fourcc.go
+++ chunk/fourcc.go
@@ -2,12 +2,15 @@ package chunk
 
 import "encoding/binary"
 
+// FourCC is a sequence of four bytes (Four-character-code) used for data format identification.
 type FourCC [4]byte
 
+// String return string value.
 func (f *FourCC) String() string {
 	return string(f[:])
 }
 
+// ToUint32 returns uint32 value.
 func (f *FourCC) ToUint32() uint32 {
 	return binary.BigEndian.Uint32(f[:])
 }
